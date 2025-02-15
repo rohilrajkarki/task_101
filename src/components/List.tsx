ab99cce7b9b85e2392a63a9368220d65964072fd
rohilrajkarki <rohilrajkarki@gmail.com>
2025-02-15 20:58:20 +0800

    Created a simple todo

---
+++ src/components/List.tsx
@@ -0,0 +1,5 @@
+const List = ({ listItem }: { listItem: string }) => {
+  return <div>{listItem}</div>;
+};
+
+export default List;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,39 @@
 import { useState } from "react";
+import List from "../components/List";
 
 const Home = () => {
   const [inputText, setInputText] = useState("");
+  const [listItems, setListItems] = useState<string[]>([]);
 
-  const submitTask = () => {
-    console.log(inputText);
+  const handleSubmit = () => {
+    const createdDate = new Date().toLocaleDateString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    console.log(inputText, createdDate);
+    setListItems([...listItems, inputText]);
+    setInputText("");
   };
+
   return (
-    <div>
-      Home
-      <input
-        placeholder="Enter a task"
-        onChange={(e) => setInputText(e.target.value)}
-        value={inputText}
-      />
-      <button onClick={submitTask} title="Create" />
-    </div>
+    <>
+      <div>
+        Home
+        <input
+          placeholder="Enter a task"
+          onChange={(e) => setInputText(e.target.value)}
+          value={inputText}
+          onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+        />
+        <button onClick={handleSubmit}>Create</button>
+      </div>
+      <div>
+        {listItems.map((item, index) => (
+          <List listItem={item} key={item + index} />
+        ))}
+      </div>
+    </>
   );
 };
 
