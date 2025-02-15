a55db0fccb8f696a307b24cdd8991bd370d6185d
rohilrajkarki <rohilrajkarki@gmail.com>
2025-02-15 22:37:24 +0800

    Updated Item infos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,16 +3,22 @@ import List from "../components/List";
 
 const Home = () => {
   const [inputText, setInputText] = useState("");
-  const [listItems, setListItems] = useState<string[]>([]);
+  const [listItems, setListItems] = useState<Item[]>([]);
 
   const handleSubmit = () => {
-    const createdDate = new Date().toLocaleDateString("en-US", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
+    const [createdDate, createdTime] = new Date()
+      .toLocaleDateString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+      .split(", ");
 
-    console.log(inputText, createdDate);
-    setListItems([...listItems, inputText]);
+    console.log(inputText, createdDate, "time", createdTime, typeof listItems);
+
+    setListItems((prev) => [
+      ...prev,
+      { id: Date.now(), text: inputText, createdDate, createdTime },
+    ]);
     setInputText("");
   };
 
@@ -29,8 +35,11 @@ const Home = () => {
         <button onClick={handleSubmit}>Create</button>
       </div>
       <div>
-        {listItems.map((item, index) => (
-          <List listItem={item} key={item + index} />
+        {listItems.map(({ id, text, createdDate, createdTime }) => (
+          <List
+            listItem={text + " " + createdDate + " " + createdTime}
+            key={id}
+          />
         ))}
       </div>
     </>
---
+++ src/types/item.d.ts
@@ -0,0 +1,6 @@
+declare interface Item {
+  id: number;
+  text: string;
+  createdDate: string;
+  createdTime: string;
+}
